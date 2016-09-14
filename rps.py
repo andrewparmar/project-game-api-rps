8295e7adf1e70f9305c7f17b32f7663b6a31e101
Andrew Parmar <andrew.parmar@gmail.com>
2016-09-13 21:06:37 -0700

    Started RPSapi class

--- rps.py
+++ rps.py
@@ -11,3 +11,17 @@ import endpoints
 from protorpc import message_types
 from protorpc import messages
 from protorpc import remote
+from models import Hello
+
+@endpoints.api(name='rock_paper_scissors', version='v1')
+class RPSApi(remote.Service):
+	"""Rock-Paper-Scissors API v1."""
+
+    @endpoints.method(message_types.VoidMessage, Hello,
+      path = "sayHello", http_method='GET', name = "sayHello")
+    def say_hello(self, request):
+      return Hello(greeting="Hello World")
+
+
+
+APPLICATION = endpoints.api_server([RPSApi])
