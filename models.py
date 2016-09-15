6a9cd6920f7c13aee92c18c5e30c38a1939033d6
Andrew Parmar <andrew.parmar@gmail.com>
2016-09-15 01:16:35 -0700

    Added create_user endpoints method

--- models.py
+++ models.py
@@ -2,8 +2,9 @@
 entities used by the game Rock-Paper-Scissors.
 """
 
+import random
 from datetime import date
-from protorps import messages
+from protorpc import messages
 from google.appengine.ext import ndb
 
 
@@ -12,69 +13,69 @@ class User(ndb.Model):
     name = ndb.StringProperty(required=True)
     email =ndb.StringProperty()
 
-
-class RPS(ndb.Model):
-	"""Game object"""
-	rounds_total = ndb.IntegerProperty(required=True)
-    rounds_remaining = ndb.IntegerProperty(required=True, default=5)
-    game_over = ndb.BooleanProperty(required=True, default=False)
-    user = ndb.KeyProperty(required=True, kind='User')
-
-    @classmethod
-    def new_game(cls, user, rounds=3):
-    	"""Creates and returns a new rps game
-    	"""
-    	game = RPS(user=user,
-    			   rounds_total=rounds
-    			   rounds_remaining=rounds,
-    			   game_over=False)
-    	game.put()
-    	return game
-
-    def end_game(self, won=False):
-        """Ends the game - if won is True, the player won. - if won is False,
-        the player lost."""
-        self.game_over = True
-        self.put()
-        # Add the game to the score 'board'
-        # score = Score(user=self.user, date=date.today(), game_won=won,
-        #               rounds_won = self.)
-        # score.put()
+class StringMessage(messages.Message):
+    """StringMessage-- outbound (single) string message"""
+    message = messages.StringField(1, required=True)
 
 
-class GameForm(messages.Message):
-    """GameForm for outbound game state information"""
-    urlsafe_key = messages.StringField(1, required=True)
-    rounds_remaining = messages.IntegerField(2, required=True)
-    game_over = messages.BooleanField(3, required=True)
-    user_name = messages.StringField(4, required=True)
-    # message = messages.StringField(4, required=True)
- 
+# class RPS(ndb.Model):
+#     """Game object"""
+#     rounds_total = ndb.IntegerProperty(required=True)
+#     rounds_remaining = ndb.IntegerProperty(required=True, default=5)
+#     game_over = ndb.BooleanProperty(required=True, default=False)
+#     user = ndb.KeyProperty(required=True, kind='User')
 
-class Score(ndb.Model):
-    """Score object"""
-    user = ndb.KeyProperty(required=True, kind='User')
-    date = ndb.DateProperty(required=True)
-    game_won = ndb.BooleanProperty(required=True)
-    rounds_won = ndb.IntegerProperty(required=True)
-    rounds_total = ndb.IntegerProperty(required=True)
+#     @classmethod
+#     def new_game(cls, user, rounds=3):
+#         """Creates and returns a new rps game
+#         """
+#         game = RPS(user=user,
+#                    rounds_total=rounds,
+#                    rounds_remaining=rounds,
+#                    game_over=False)
+#         game.put()
+#         return game
+
+#     def end_game(self, won=False):
+#         """Ends the game - if won is True, the player won. - if won is False,
+#         the player lost."""
+#         self.game_over = True
+#         self.put()
+#         # Add the game to the score 'board'
+#         # score = Score(user=self.user, date=date.today(), game_won=won,
+#         #               rounds_won = self.)
+#         # score.put()
 
 
-class ScoreForm(message.Message):
-    """ScoreForm for outbound Score information"""
-    user_name = messages.StringField(1, required=True)
-    date = messages.StringField(2, required=True)
-    game_won = messages.BooleanField(3, required=True)
-    rounds_won = messages.IntegerField(4, required=True)
-    rounds_total = messsages.IntegerProperty(5, required=True)
+# class GameForm(messages.Message):
+#     """GameForm for outbound game state information"""
+#     urlsafe_key = messages.StringField(1, required=True)
+#     rounds_remaining = messages.IntegerField(2, required=True)
+#     game_over = messages.BooleanField(3, required=True)
+#     user_name = messages.StringField(4, required=True)
+#     # message = messages.StringField(4, required=True)
 
 
-class MoveOptions(messages.Enum):
-    """RPS - enumeration value"""
-    ROCK = 1
-    PAPER = 2
-    SCISSOR = 3
+# class Score(ndb.Model):
+#     """Score object"""
+#     user = ndb.KeyProperty(required=True, kind='User')
+#     date = ndb.DateProperty(required=True)
+#     game_won = ndb.BooleanProperty(required=True)
+#     rounds_won = ndb.IntegerProperty(required=True)
+#     rounds_total = ndb.IntegerProperty(required=True)
 
-class Hello(messages.Message):
-    """String that stores a message."""
-    greeting = messages.StringField(1)
+
+# class ScoreForm(message.Message):
+#     """ScoreForm for outbound Score information"""
+#     user_name = messages.StringField(1, required=True)
+#     date = messages.StringField(2, required=True)
+#     game_won = messages.BooleanField(3, required=True)
+#     rounds_won = messages.IntegerField(4, required=True)
+#     rounds_total = messsages.IntegerProperty(5, required=True)
+
+
+# class MoveOptions(messages.Enum):
+#     """RPS - enumeration value"""
+#     ROCK = 1
+#     PAPER = 2
+#     SCISSOR = 3
--- rps.py
+++ rps.py
@@ -5,23 +5,51 @@ rps-api.py -- Udacity Game-API server-side Python App Engine API;
     uses Google Cloud Endpoints
 """
 
-__author__ = 'andrew.parmar@gmail.com'
+# __author__ = 'andrew.parmar@gmail.com'
 
 import endpoints
 from protorpc import message_types
 from protorpc import messages
 from protorpc import remote
-from models import Hello
+
+from models import User
+from models import StringMessage
+
+
+USER_REQUEST = endpoints.ResourceContainer(user_name=messages.StringField(1),
+                                           email=messages.StringField(2))
+
+
+class Hello(messages.Message):
+    """String that stores a message."""
+    greeting = messages.StringField(1)
+
 
 @endpoints.api(name='rock_paper_scissors', version='v1')
 class RPSApi(remote.Service):
-	"""Rock-Paper-Scissors API v1."""
+    """Rock-Paper-Scissors API v1."""
 
-    @endpoints.method(message_types.VoidMessage, Hello,
-      path = "sayHello", http_method='GET', name = "sayHello")
-    def say_hello(self, request):
-      return Hello(greeting="Hello World")
+    @endpoints.method(message_types.VoidMessage,
+                      Hello,
+                      path='sayHello',
+                      http_method='GET',
+                      name='sayHello')
+    def say_hello(self, unused_request):
+        return Hello(greeting="Hello World")
 
+    @endpoints.method(request_message=USER_REQUEST,
+                      response_message=StringMessage,
+                      path='user',
+                      name='create_user',
+                      http_method='POST')
+    def create_user(self, request):
+        """Create a User. Requires a unique username"""
+        if User.query(User.name == request.user_name).get():
+            raise endpoints.ConflictException(
+                'A User with that name already exists!')
+        user = User(name=request.user_name, email=request.email)
+        user.put()
+        return StringMessage(message='User {} created!'.format(request.user_name))
 
 
 APPLICATION = endpoints.api_server([RPSApi])
